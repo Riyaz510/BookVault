e0f7a2d01c72b308c94ab7b70d9455423156bed0
Riyaz510 <riyazrocky2005@gmail.com>
2026-09-22 19:16:24 +0530

    Configure ApplicationDbContext with EF Core and SQL Server

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+
+using BookVault.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookVault.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
