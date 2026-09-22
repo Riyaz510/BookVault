ff232c7e6bd9856d3a5c1933576d7dc621e925c4
Riyaz510 <riyazrocky2005@gmail.com>
2026-09-22 19:16:10 +0530

    Add home page with BookVault hero section and feature cards

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+using BookVault.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookVault.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = System.Diagnostics.Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace BookVault.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
